b191fa4cde8ca519dc5988f194813a5fd130f8c1
Asad Shahin Moghadam <Asad.shmoghadam@gmail.com>
2020-11-02 15:06:01 -0500

    Fixing socket issue

--- public/js/Dashboard/PanelSensorFeed.js
+++ public/js/Dashboard/PanelSensorFeed.js
@@ -15,7 +15,7 @@ class SensorFeed extends DashboardPanelSensorData {
   }
 
   socketConnect(chart) {
-    if (!window.socket) window.socket = io.connect("https://cogbim.azurewebsites.net/",{transports:['websocket','xhr-polling']});
+    if (!window.socket) window.socket = io.connect("http://localhost:8080", {transports:['websocket','xhr-polling']});
 
     window.socket.on("SensorStream", (data) => {
       this.addData(chart, data);
