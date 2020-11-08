fe348a6c73bf0cf7c925ddeadf29ef52223d1f97
Asad Shahin Moghadam <Asad.shmoghadam@gmail.com>
2020-11-07 19:33:45 -0500

    Websocket fix

--- public/js/Dashboard/PanelSensorFeed.js
+++ public/js/Dashboard/PanelSensorFeed.js
@@ -15,8 +15,8 @@ class SensorFeed extends DashboardPanelSensorData {
   }
 
   socketConnect(chart) {
-    if (!window.socket) window.socket = io.connect("http://localhost:8080", {transports:['websocket','xhr-polling']});
-
+    //if (!window.socket) window.socket = io.connect("http://localhost:8080", {transports:['websocket','xhr-polling']});
+    if (!window.socket) window.socket = io.connect("http://localhost:8080");
     window.socket.on("SensorStream", (data) => {
       this.addData(chart, data);
     });
