e628bdbff1d5e9cf5111947591d3b05ec8ad7b06
Asad Shahin Moghadam <Asad.shmoghadam@gmail.com>
2020-11-07 19:34:37 -0500

    change oss mode to temporary

--- routes/oss.js
+++ routes/oss.js
@@ -59,7 +59,7 @@ router.get('/buckets', async (req, res, next) => {
 router.post('/buckets', async (req, res, next) => {
     let payload = new PostBucketsPayload();
     payload.bucketKey = config.credentials.client_id.toLowerCase() + '-' + req.body.bucketKey;
-    payload.policyKey = 'transient'; // expires in 24h
+    payload.policyKey = 'temporary'; // expires in 24h
     try {
         // Create a bucket using [BucketsApi](https://github.com/Autodesk-Forge/forge-api-nodejs-client/blob/master/docs/BucketsApi.md#createBucket).
         await new BucketsApi().createBucket(payload, {}, req.oauth_client, req.oauth_token);
